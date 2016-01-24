160d987bdb3f6b66740046eac28507ae564f8d0a
Micah <micahgester@gmail.com>
2016-01-23 22:01:39 -0800

    website-push
    first push

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*var starWars = prompt("What is your first name");
+var lastName = prompt(" What is your last name");
+var momName =  prompt(" Mom's maiden name");
+var city = prompt("your city");
+var car = prompt("car owned");
+var food = prompt("last food eaten");
+
+var newName = starWars.substring(0,3) + lastName.substring(0,2) ;
+var newLastname = momName.substring(0,2) + city.substring(0,3);
+var honor = [lastname.substring(3,5)];
+
+
+console.log (newName, honor);
+
+*/
